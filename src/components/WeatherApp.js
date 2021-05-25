0682cf06bad49b7c4585e38f687ce35eedc7a89e
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-05-25 16:15:55 -0400

    style fix

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -22,7 +22,9 @@ function WeatherApp({
   }, [location, setCoords]);
 
   return (
-    <div style={{ width: "100%", backgroundColor: "lightgray" }}>
+    <div
+      style={{ width: "100%", height: "100%", backgroundColor: "lightgray" }}
+    >
       <div
         className="App"
         style={{
